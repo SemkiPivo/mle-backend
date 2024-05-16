ad6deee9b21df1340af7d126dc38e83e9524bf6d
Kirill <blacklight310398@gmail.com>
2024-05-16 11:41:45 +0300

    Initial commit

---
+++ database/migrations/2024_05_16_083607_add_city_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Location;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignIdFor(Location::class);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeignIdFor(Location::class);
+            $table->dropColumn('location_id');
+        });
+    }
+};
